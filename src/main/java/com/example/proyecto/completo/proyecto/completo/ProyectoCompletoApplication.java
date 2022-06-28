5d41d84fdffb18cc0dbe918e1a2ef54b4bc546f9
Luisa Fernanda Betancur Loiaza <lbetancu@emeal.nttdata.com>
2022-06-28 11:44:06 -0500

    se sube proyecto

---
+++ src/main/java/com/example/proyecto/completo/proyecto/completo/ProyectoCompletoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto.completo.proyecto.completo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCompletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto/completo/proyecto/completo/ProyectoCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto.completo.proyecto.completo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
